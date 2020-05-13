f079ad7f23b0e2114cc512b31cd748d0ab83ecc5
Sjors Wijsman <sjorswijsman@gmail.com>
2020-05-13 16:34:13 +0200

    First version of prototype
    Created prototype & wireflow

--- index.js
+++ index.js
@@ -6,34 +6,71 @@ let path = require('path')
 
 const port = 3000;
 
-let public = '/public/'
+let publicDir = '/public'
 
 // Open listen port
 app.listen(port, function () {
   console.log(`Example app listening at http://localhost:${port}`)
 });
 
-// app.use(express.static('public'))
-
 // Set EJS as view engine
 app.set('view engine', 'ejs');
 
-app.get(['/', '/homepage', '/index'], function (req, res) {
-  let page = path.join(__dirname + public + 'homepage')
-  let location = "Homepage";
-  let list = ["One", "Two", "Three", "Four"];
-  res.render(page, {location: location, list: list});
+app.use(express.static('public'));
+
+app.get(['/', '/homepage', '/index', '/discover'], function (req, res) {
+  let page = path.join(__dirname + publicDir + '/discover')
+
+  let pageTitle = "Generic Dating App - Discover";
+
+  let opusActive = false;
+
+  res.render(page, {title: pageTitle, opus: opusActive});
 })
 
+app.get(['/discover-opus'], function (req, res) {
+  let page = path.join(__dirname + publicDir + '/discover')
+
+  let pageTitle = "Opus - Discover";
+
+  let opusActive = true;
+
+  res.render(page, {title: pageTitle, opus: opusActive});
+})
+
+app.get('/profile', function (req, res) {
+  let page = path.join(__dirname + publicDir + '/profile')
+
+  let pageTitle = "Generic Dating App - Profile";
+
+  let opusActive = false;
+
+  res.render(page, {title: pageTitle, opus: opusActive});
+})
+
+app.get(['/profile-opus'], function (req, res) {
+  let page = path.join(__dirname + publicDir + '/profile')
+
+  let pageTitle = "Opus - Profile";
+
+  let opusActive = true;
+
+  res.render(page, {title: pageTitle, opus: opusActive});
+})
+
+
+
 app.get('/users', function (req, res) {
-  let page = path.join(__dirname + public + 'users');
+  let page = path.join(__dirname + publicDir + '/users');
   let location = "Users";
+
   let users = ["Dallas", "Chains", "Hoxton", "Wolf"];
+
   res.render(page, {location: location, users: users});
 })
 
 app.get('/video', function (req, res) {
-  res.sendFile(path.join(__dirname + public + 'video.mp4'));
+  res.sendFile(path.join(__dirname + publicDir + '/video.mp4'));
 })
 
 app.get('/profile/:profileID', function (req, res) {
@@ -41,11 +78,12 @@ app.get('/profile/:profileID', function (req, res) {
 })
 
 
+
 app.use(function (req, res, next) {
-  res.status(404).send("Sorry can't find that!")
+  res.status(404).send("Sorry can't find that! (Error code 404)")
 })
 
 app.use(function (err, req, res, next) {
   console.error(err.stack)
-  res.status(500).send('Something broke!')
+  res.status(500).send('Something broke! (Error code 500)')
 })
