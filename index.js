3c3cac832bbc92e084c0a256dbbd745e136120f6
Sjors Wijsman <sjorswijsman@gmail.com>
2020-05-19 17:45:52 +0200

    Changed port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser')
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 const opus = {
